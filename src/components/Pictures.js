c5240f674da8e446a0d417330d19f28fd16ebffb
Helen <yuhan.yangg@gmail.com>
2021-08-27 19:13:52 +0800

    click the photo to its website

--- src/components/Pictures.js
+++ src/components/Pictures.js
@@ -5,14 +5,10 @@ const Pictures = ({ photos }) => {
     <div className="picture">
       <p>{photos.photographer}</p>
       <div className="imageContainer">
-        <img src={photos.src.large} alt="" />
-      </div>
-      <p>
-        Download Image:
         <a target="_blank" href={photos.src.large}>
-          Click Here
+          <img src={photos.src.large} alt="" />
         </a>
-      </p>
+      </div>
     </div>
   );
 };
